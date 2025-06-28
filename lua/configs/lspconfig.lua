1db8d63cbfd9774d59692d765c1b5cb08c392a3c
John Zhou <johnzhou2004@outlook.com>
2025-06-28 01:36:40 -0400

    back to working, and have extra commands for lsp and telescope POG

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -7,10 +7,35 @@ local lspconfig = require "lspconfig"
 local servers = { "clangd", "pylsp" } -- "pyright", "html", "cssls" 
 local nvlsp = require "nvchad.configs.lspconfig"
 
+local lsp_keymaps = function(bufnr)
+  -- go to different defintion of defintion, types, and implementations
+  vim.keymap.set("n", "K", vim.lsp.buf.hover, {buffer=bufnr})
+  vim.keymap.set("n", "gd", vim.lsp.buf.definition, {buffer=bufnr, desc="go to definition"})
+  vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, {buffer=bufnr, desc="go to type definition"})
+  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, {buffer=bufnr, desc="go to implementation"})
+  
+  -- go through diagnostics
+  vim.keymap.set("n", "<leader>df", vim.diagnostic.goto_next, {buffer=bufnr, desc="go to next diagnostic"})
+  vim.keymap.set("n", "<leader>dp", vim.diagnostic.goto_prev, {buffer=bufnr, desc="go to prev diagnostic"})
+
+  -- list diagnostics of workspace
+  vim.keymap.set("n", "<leader>dl", require("telescope.builtin").diagnostics, {buffer=bufnr, desc="list diagnostics in ran script"})
+
+  -- finds instances of this function in the workflow
+  vim.keymap.set("n", "<leader>fr", require("telescope.builtin").lsp_references, {buffer=bufnr, desc="find all instances of this function in folder"})
+end
+
+
+-- Wrap the original on_attach
+local function custom_on_attach(client, bufnr)
+  nvlsp.on_attach(client, bufnr) -- Call NvChad's default on_attach
+  lsp_keymaps(bufnr)             -- Then your custom keymaps
+end
+
 -- lsps with default config
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
-    on_attach = nvlsp.on_attach,
+    on_attach = custom_on_attach,
     on_init = nvlsp.on_init,
     capabilities = nvlsp.capabilities,
   }
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -12,32 +12,6 @@ return {
       require "configs.lspconfig"
     end,
   },
-    config = function()
-      local capabilities = require("configs.lspconfig")
-      local lsp_keymaps = function()
-        -- go to different defintion of defintion, types, and implementations
-        vim.keymap.set("n","K",vim.lsp.buf.hover,{buffer=0})
-        vim.keymap.set("n","gd", vim.lsp.buf.definition, {buffer=0})
-        vim.keymap.set("n","gt", vim.lsp.buf.type_definition, {buffer=0})
-        vim.keymap.set("n","gi", vim.lsp.buf.implementaton, {buffer=0})
-        -- go through diagnostics
-        vim.keymap.set("n","<leader>df", vim.diagnostic.goto_next, {buffer=0})
-        vim.keymap.set("n","<leader>dp", vim.diagnostic.goto_prev, {buffer=0})
-        -- list diagnostics of workspace
-        vim.keymap.set("n","<leader>dl", "<cmd>Telescope diagnostic<cr>", {buffer=0})
-        -- finds instances of this function in the workflow
-        vim.keymap.set("n","<leader>fr", require("telescope.builtin").lsp_references, {buffer=0})
-      end
-
-    -- Set up the config for every lanaguage server
-    require"lspconfig".clangd.setup{
-      capabilities = capabilities 
-      on_attach = lsp_keymaps
-    }
-    require"lspconfig".pylsp.setup{
-      capabilities = capabilities 
-      on_attach = lsp_keymaps
-    }
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
   -- 	opts = {
